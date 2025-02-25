2b91b55a01f7e72a202c2b3167b863d7558e3e52
Sandesh Kumar <164940124+imaluffy@users.noreply.github.com>
2025-02-26 04:30:14 +0530

    Update BaseTrainRepository.java

--- src/main/java/com/trs/trs_admin_service/repo/BaseTrainRepository.java
+++ src/main/java/com/trs/trs_admin_service/repo/BaseTrainRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BaseTrainRepository extends JpaRepository<TrainStopBase, Long> {
+public interface BaseTrainRepository extends JpaRepository<TrainStopBase, Integer> {
 }
