810c19a58b69b61bb89efdbcf031139ec71ed0e5
Dheeraj <djha199515@gmail.com>
2025-02-25 22:19:33 +0530

    delete train logic

--- src/main/java/com/trs/trs_admin_service/repo/TrainRepository.java
+++ src/main/java/com/trs/trs_admin_service/repo/TrainRepository.java
@@ -4,9 +4,7 @@ import com.trs.trs_admin_service.model.Train;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface TrainRepository extends JpaRepository<Train, Long> {
-    Optional<Train> findByTrainNumber(Integer trainNumber);
+    Train findByTrainNumber(Integer trainNumber);
 }
